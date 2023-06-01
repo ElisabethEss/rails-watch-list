0988f26cfe7fa3e1c38fe06677fe48fbd7841701
Elisabeth Essbaumer <essbaumer.elisabeth@web.de>
2023-06-01 16:39:44 +0200

    added 3 models

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,52 @@
+class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to @movie, notice: "Movie was successfully created"
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @movie.update(movie_params)
+      redirect_to @movie, notice: 'Movie was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    if @movie.bookmarks.any?
+      redirect_to @movie, alert: 'Cannot delete movie with associated bookmarks.'
+    else
+      @movie.destroy
+      redirect_to movies_url, notice: 'Movie was successfully destroyed.'
+    end
+  end
+
+  private
+
+  def set_movie
+    @movie = Movie.find(params[:id])
+  end
+
+  def movie_params
+    params.require(:movie).permit(:title, :overview, :poster_url, :rating)
+  end
+end
